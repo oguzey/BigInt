b1f4dd9c241fd7466c04b78a21e5b07d9532fc84
Oleh Huzei <oleg.guz.rus@gmail.com>
2016-05-10 19:40:53 +0300

    Added example of working with numbers

--- main.cpp
+++ main.cpp
@@ -6,6 +6,34 @@
 
 
 int main() {
-	LOG("Hello, world!");
+	// create default BigInt
+	BigInt biDefault;
+
+	// create BigInt from hex string
+	BigInt biNumber("AAAFFF2428348");
+
+	// example of addition of two numbers
+	// result will be written in first number
+
+	BigInt a, b;
+
+	a.setZero();
+	b.setNumber(1);
+	a.add(b);
+
+	// example of multiplication of two numbers with modular reduction
+
+	BigInt x, y, m, ret;
+
+	x.fromString("4cd");
+	y.fromString("16a0");
+	m.fromString("11bbf");
+
+	m.initModularReduction();
+	x.mulMont(y, m, ret);
+	m.shutDownModularReduction();
+
+	LOG("Result of multiplication is {}", ret.toString());
+
 	return 0;
 }
